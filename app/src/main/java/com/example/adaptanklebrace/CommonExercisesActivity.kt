1a755e5d43daeb6e9d966105357b41f8de460368
SeanBakker <sbakker0202@gmail.com>
2025-01-06 18:20:24 -0500

    Updated common exercises list

--- app/src/main/java/com/example/adaptanklebrace/CommonExercisesActivity.kt
+++ app/src/main/java/com/example/adaptanklebrace/CommonExercisesActivity.kt
@@ -1,9 +1,11 @@
 package com.example.adaptanklebrace
 
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
 import android.widget.LinearLayout
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.databinding.BindingAdapter
@@ -17,6 +19,7 @@ class CommonExercisesActivity : AppCompatActivity() {
     private lateinit var quickLinksLayout: LinearLayout
     private lateinit var exercisePagesAdapter: ExercisePagesAdapter
 
+    @RequiresApi(Build.VERSION_CODES.Q)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_common_exercises)
@@ -50,6 +53,11 @@ class CommonExercisesActivity : AppCompatActivity() {
         when (view.id) {
             R.id.btn_exercise_1 -> viewPager.currentItem = 0
             R.id.btn_exercise_2 -> viewPager.currentItem = 1
+            R.id.btn_exercise_3 -> viewPager.currentItem = 2
+            R.id.btn_exercise_4 -> viewPager.currentItem = 3
+            R.id.btn_exercise_5 -> viewPager.currentItem = 4
+            R.id.btn_exercise_6 -> viewPager.currentItem = 5
+            R.id.btn_exercise_7 -> viewPager.currentItem = 6
         }
     }
 
@@ -79,9 +87,15 @@ class CommonExercisesActivity : AppCompatActivity() {
         @JvmStatic
         fun setImageFromResourceId(view: ImageView, imageId: Int?) {
             imageId?.let {
+                //todo: add exercise images
                 val drawableResId = when (it) {
                     1 -> R.drawable.baseline_directions_walk_24 // exercise #1
                     2 -> R.drawable.baseline_directions_run_24 // exercise #2
+                    3 -> R.drawable.baseline_error_24 // exercise #3
+                    4 -> R.drawable.baseline_error_24 // exercise #4
+                    5 -> R.drawable.baseline_error_24 // exercise #5
+                    6 -> R.drawable.baseline_error_24 // exercise #6
+                    7 -> R.drawable.baseline_error_24 // exercise #7
                     else -> R.drawable.baseline_error_24 // error
                 }
                 view.setImageResource(drawableResId)
--- app/src/main/java/com/example/adaptanklebrace/enums/ExerciseType.kt
+++ app/src/main/java/com/example/adaptanklebrace/enums/ExerciseType.kt
@@ -5,8 +5,83 @@ import androidx.annotation.RequiresApi
 import com.example.adaptanklebrace.data.Exercise
 
 enum class ExerciseType(val exerciseName: String, val description: String, val steps: String, val imageId: Int) {
-    ANKLE_STRETCH("Ankle Stretch", "Description of ankle stretch", "Steps to do it", 1),
-    HEEL_RAISE("Heel Raise", "Description of heel raise", "Steps to do it", 2);
+    PLANTAR_FLEXION(
+        "Plantar Flexion",
+        "A stretch that targets the calf and foot muscles to improve flexibility and range of motion.",
+        """
+        1. Sit down with your legs extended straight in front of you.
+        2. Flex your foot downward, pointing your toes as far as possible.
+        3. Hold the stretch for a specified duration and repeat according to the number of reps.
+        4. Repeat for the specified number of sets.
+        """.trimIndent(),
+        1
+    ),
+    DORSIFLEXION(
+        "Dorsiflexion",
+        "This exercise improves ankle flexibility and strengthens the muscles on the front of the lower leg.",
+        """
+        1. Sit down with your legs extended straight in front of you.
+        2. Flex your foot upward, bringing your toes toward your shin.
+        3. Hold the stretch for a specified duration and repeat according to the number of reps.
+        4. Repeat for the specified number of sets.
+        """.trimIndent(),
+        2
+    ),
+    INVERSION(
+        "Inversion",
+        "An exercise that targets the ankle's ability to move inward, strengthening the muscles that stabilize the ankle.",
+        """
+        1. Sit down with your legs extended straight in front of you.
+        2. Slowly rotate your foot inward, working the muscles on the inside of your ankle.
+        3. Hold the stretch for a specified duration and repeat according to the number of reps.
+        4. Repeat for the specified number of sets.
+        """.trimIndent(),
+        3
+    ),
+    EVERSION(
+        "Eversion",
+        "Strengthens the muscles on the outside of the ankle to improve lateral stability and flexibility.",
+        """
+        1. Sit down with your legs extended straight in front of you.
+        2. Slowly rotate your foot outward, working the muscles on the outside of your ankle.
+        3. Hold the stretch for a specified duration and repeat according to the number of reps.
+        4. Repeat for the specified number of sets.
+        """.trimIndent(),
+        4
+    ),
+    RANGE_OF_MOTION(
+        "Range of Motion (ROM)",
+        "An exercise to monitor improvements in flexibility of the ankle joint.",
+        """
+        1. Sit comfortably and extend one leg straight in front of you.
+        2. Slowly move your foot in circles, first in the clockwise direction.
+        3. Perform the specified number of reps (rotations).
+        4. After a short break, repeat the exercise in the counterclockwise direction.
+        """.trimIndent(),
+        5
+    ),
+    GAIT_SYMMETRY(
+        "Gait Symmetry",
+        "An exercise to monitor improvements in the recovery of gait of the ankle joint.",
+        """
+        1. Stand with your feet hip-width apart.
+        2. Walk in a straight line, with each step of equal length.
+        3. Perform the specified number of reps (steps).
+        4. After a short break, repeat for the specified number of sets.
+        """.trimIndent(),
+        6
+    ),
+    IMPACT_FORCE(
+        "Impact Force",
+        "An exercise to monitor improvements in the body's ability to absorb shock during the walking cycle.",
+        """
+        1. Stand with your feet hip-width apart.
+        2. Walk in a straight line, with each step of equal length.
+        3. Perform the specified number of reps (steps).
+        4. After a short break, repeat for the specified number of sets.
+        """.trimIndent(),
+        7
+    );
 
     companion object {
         // Convert the enum values to a list of Exercise objects
